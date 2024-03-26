659b87bcd5bea2884866ac1bc023f266305d0271
David Kopczynski <david.kop.dk@gmail.com>
2024-03-26 18:12:20 +0100

    Experimental update of setup to generate new .env.nix for usage in other files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 cd "$(dirname "$0")"
 
+# ---------- Generate env ---------- #
+
+# Skip if .env.nix already exists
+if [ -f .env.nix ]; then
+  echo ".env.nix already exists. Skipping generation of environment variables."
+else
+
+  # Get environment variables for NixOS setup
+  echo "Generation of environment variables for NixOS setup:"
+  read -p "Host: " host
+
+  # Create .env.nix with all necessary environment variables
+  touch .env.nix
+  echo "{
+    root = \"$PWD\";
+    host = \"$host\";
+  }" > .env.nix
+fi
+
 # ---------- NixOS ---------- #
 
 # Generate configuration.nix with current location via pwd
