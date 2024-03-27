c1a2b41ba622f5ccfff7d40fe58a2cac1e02e68e
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-03-27 19:30:39 +0100

    Added possibility to temporarily add unfree packages

--- setup.sh
+++ setup.sh
@@ -55,6 +55,10 @@ chmod 644 nixos/hardware-configuration.nix
 ln nixos/configuration.nix /etc/nixos/configuration.nix
 ln nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
+# Allow unfree packages in command line
+mkdir -p ~/.config/nixpkgs
+echo "{ allowUnfree = true; }" >> ~/.config/nixpkgs/config.nix
+
 # Add all missing channels
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
 sudo nix-channel --update
