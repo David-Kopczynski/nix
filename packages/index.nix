80cca7f2bdcd652f2f3a694b83ef04447d3ebfa9
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-03-30 00:33:14 +0100

    Fixed discord wayland

--- packages/index.nix
+++ packages/index.nix
@@ -26,9 +26,14 @@
         spotify
         pdfarranger
 
-        (pkgs.writeShellScriptBin "discord" ''
-            exec ${pkgs.discord}/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland
-        '')
+        # Wayland temp fix
+        tela-circle-icon-theme
+        (pkgs.makeDesktopItem {
+            name = "discord";
+            exec = "${pkgs.discord}/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland";
+            desktopName = "Discord";
+            icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/discord.svg";
+        })
 
         # ---------- Games ---------- #
         prismlauncher
