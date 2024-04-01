7da99eab2ab734b8c55299c4cab86e8e1edd771b
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-04-01 11:55:03 +0200

    Removed unnecessary packages from gnome

--- hosts/default/window.nix
+++ hosts/default/window.nix
@@ -7,4 +7,18 @@
     # Enable gnome desktop
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
+
+    # Remove unnecessary packages from gnome
+    environment.gnome.excludePackages = (with pkgs; [
+        gnome-tour
+        gnome.gnome-music
+        gnome.gnome-terminal
+        gnome.gedit
+        gnome.epiphany
+        gnome.geary
+        gnome.tali
+        gnome.iagno
+        gnome.hitori
+        gnome.atomix
+    ]);
 }
