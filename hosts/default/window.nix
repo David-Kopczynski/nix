e8ee0d028da3789854a58eee24bc4068e62ac0b3
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-04-07 11:05:14 +0200

    Added dark mode for QT with gnome

--- hosts/default/window.nix
+++ hosts/default/window.nix
@@ -8,4 +8,11 @@
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.displayManager.gdm.wayland = false; # This is an patch for NVIDIA as wayland crashes
     services.xserver.desktopManager.gnome.enable = true;
+
+    # Force QT applications to use dark theme
+    qt = {
+        enable = true;
+        platformTheme = "gnome";
+        style = "adwaita-dark";
+    };
 }
