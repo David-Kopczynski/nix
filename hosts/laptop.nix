266e42e3a9ec138ce3db87d3b4ff9d7960179a5c
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-05-02 14:12:10 +0200

    Added power management for better battery life

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -18,6 +18,21 @@
     driSupport32Bit = true;
   };
 
+  # Better battery life
+  powerManagement.powertop.enable = true;
+
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+
   # Enable firmware updates
   services.fwupd.enable = true;
 }
