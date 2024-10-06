91b1f17dcdee28a967a8b15780137692806f21a7
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-06 15:40:24 +0200

    Ported ssh config to home manager

--- install/ssh.nix
+++ install/ssh.nix
@@ -1,5 +1,101 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.openssh.enable = true;
+
+  home-manager.users.user.programs.ssh = {
+    inherit (config.services.openssh) enable;
+
+    matchBlocks = {
+      # # # # # # # # # # # # # # # # # # # # # # # # # # #
+      #         Cloudflared Reverse Proxy for SSH         #
+      # # # # # # # # # # # # # # # # # # # # # # # # # # #
+      "proxy" = {
+        hostname = "ssh.davidkopczynski.com";
+        proxyCommand = "cloudflared access ssh --hostname ssh.davidkopczynski.com";
+        user = "root";
+      };
+      "proxmox" = {
+        hostname = "10.1.0.0";
+        proxyJump = "proxy";
+        user = "root";
+        localForwards = [
+          {
+            bind.port = 8006;
+            host.address = "localhost";
+            host.port = 8006;
+          }
+        ];
+      };
+      "kuma" = {
+        hostname = "10.0.1.10";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "base" = {
+        hostname = "10.0.2.10";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "pihole" = {
+        hostname = "10.0.3.10";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "printer" = {
+        hostname = "10.0.3.20";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "home" = {
+        hostname = "10.0.3.30";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "cloud" = {
+        hostname = "10.0.4.10";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "archive" = {
+        hostname = "10.0.4.20";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "photos" = {
+        hostname = "10.0.4.30";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "pdf" = {
+        hostname = "10.0.4.40";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "bachelor" = {
+        hostname = "10.0.5.10";
+        proxyJump = "proxy";
+        user = "root";
+      };
+      "test" = {
+        hostname = "10.0.9.00";
+        proxyJump = "proxy";
+        user = "root";
+      };
+
+      # # # # # # # # # # # # # # # # # # # # # # # # # # #
+      #                   PSP Websites                    #
+      # # # # # # # # # # # # # # # # # # # # # # # # # # #
+      "psp-website" = {
+        hostname = "psp-website.embedded.rwth-aachen.de";
+        user = "psp";
+      };
+      "psp-website-dev" = {
+        hostname = "psp-website-dev.embedded.rwth-aachen.de";
+        user = "psp";
+      };
+    };
+
+    userKnownHostsFile = "~/.ssh/known_hosts ${config.root}/install/ssh_known_hosts";
+  };
 }
