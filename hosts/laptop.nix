24f9942b6f810ff0f9af0d69114536c8a25e7740
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-19 01:51:29 +0200

    Removed cpu auto frequency management

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -25,18 +25,6 @@
   # Better battery life
   powerManagement.powertop.enable = true;
 
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-  };
-
   # Enable firmware updates
   services.fwupd.enable = true;
 }
