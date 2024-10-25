9858812ecd3516f82bac3e44c4bacc4ede9df436
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-26 00:35:45 +0200

    Added profile picture

--- install/gnome.themes.nix
+++ install/gnome.themes.nix
@@ -9,4 +9,17 @@
       picture-uri-dark = "file://${config.root}/resources/gnome/wallpaper-dark.jpg";
     };
   };
+
+  # Profile picture (workaround)
+  system.activationScripts.script.text = ''
+    mkdir -p /var/lib/AccountsService/{icons,users}
+    cp ${config.root}/resources/gnome/profile-picture.png /var/lib/AccountsService/icons/user
+    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/user\n" > /var/lib/AccountsService/users/user
+
+    chown root:root /var/lib/AccountsService/users/user
+    chmod 0600 /var/lib/AccountsService/users/user
+
+    chown root:root /var/lib/AccountsService/icons/user
+    chmod 0444 /var/lib/AccountsService/icons/user
+  '';
 }
