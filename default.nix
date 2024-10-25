661031e52ae70a7f8f4f0bfbfb6cc838528c6342
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-25 22:25:20 +0200

    Updated auto-include to work for multiple directories

--- default.nix
+++ default.nix
@@ -7,12 +7,11 @@
       ./secrets.nix # Secrets
     ]
 
-    # Automatically include default system configuration from ./hosts/default
-    ++ builtins.map (n: toString ./hosts/default + "/${n}") (builtins.filter (lib.strings.hasSuffix ".nix") (builtins.attrNames (builtins.readDir ./hosts/default)))
-
-    # Automatically include all channel configs from ./channels
-    ++ builtins.map (n: toString ./channels + "/${n}") (builtins.filter (lib.strings.hasSuffix ".nix") (builtins.attrNames (builtins.readDir ./channels)))
-
-    # Automatically include all install configs from ./install
-    ++ builtins.map (n: toString ./install + "/${n}") (builtins.filter (lib.strings.hasSuffix ".nix") (builtins.attrNames (builtins.readDir ./install)));
+    ++ builtins.concatMap (dir: builtins.map (n: toString dir + "/${n}") (builtins.attrNames (builtins.readDir dir)))
+      [
+        # Automatically include all configuration from:
+        ./channels
+        ./hosts/default
+        ./install
+      ];
 }
