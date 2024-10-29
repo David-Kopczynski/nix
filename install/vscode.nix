3c5788421cb8840442089c4fc909f5a6296d37fd
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-29 01:20:00 +0100

    Updated VSCode to use all system packages in fhs mode; Fixed SSH permission issues

--- install/vscode.nix
+++ install/vscode.nix
@@ -1,7 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs.unstable; [ vscode.fhs ];
+  home-manager.users.user.programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; config.environment.systemPackages);
+  };
+
+  # Fix SSH config permissions for FHS
+  home-manager.users.user.home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
 
   # Remove gnome default application
   environment.gnome.excludePackages = with pkgs; [ gnome-text-editor ];
