0eccf395fad76fd7fe07f8f4a74cadd87e5d5390
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-29 11:13:29 +0100

    Added shorter delay timeouts in case of non-stopping applications in case of shutdown

--- hosts/default/system.nix
+++ hosts/default/system.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ config, ... }:
 
 {
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Prevent shutdown delays
+  systemd.extraConfig = "DefaultTimeoutStopSec=10s";
+  systemd.user.extraConfig = config.systemd.extraConfig;
 
   system.stateVersion = "23.11";
 }
