331806107a3a0ca1b1fc7cf76bd9ddebeffc16f6
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-10-29 01:51:01 +0100

    Removed shortcuts from workstation

--- install/gnome.interface.nix
+++ install/gnome.interface.nix
@@ -78,12 +78,6 @@ with lib.gvariant;
             "net.lutris.Lutris.desktop"
             "org.prismlauncher.PrismLauncher.desktop"
             "r2modman.desktop"
-
-            "PAYDAY 2.desktop"
-            "Lethal Company.desktop"
-            "Sons Of The Forest.desktop"
-            "Enshrouded.desktop"
-            "Content Warning.desktop"
           ]
         )
       ];
