f4c5730bb543b8b5b7ea49aeea970d3a0da312c3
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-11-17 02:53:10 +0100

    Added hyperion (ambient light service)

---
+++ install/hyperion.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+lib.mkIf (config.host == "workstation") {
+  environment.systemPackages = with pkgs; [ hyperion-ng ];
+
+  systemd.services.hyperiond = {
+    enable = true;
+
+    description = "Hyperion.ng Service";
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+
+    serviceConfig = {
+      User = "root";
+      Group = "root";
+      ExecStart = "${pkgs.hyperion-ng}/bin/hyperiond --service";
+      TimeoutStopSec = "10";
+      Restart = "on-failure";
+    };
+  };
+}
