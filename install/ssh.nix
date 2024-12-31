05ee25115f112508332a7ded5281d5972c2ad218
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-12-31 15:35:07 +0100

    Updated SSH targets with new server structure

--- install/ssh.nix
+++ install/ssh.nix
@@ -18,90 +18,23 @@
       # # # # # # # # # # # # # # # # # # # # # # # # # # #
       #               Reverse Proxy for SSH               #
       # # # # # # # # # # # # # # # # # # # # # # # # # # #
-      "proxy" = {
-        hostname = "ssh.davidkopczynski.com";
-        user = "root";
-      };
       "proxmox" = {
         hostname = "10.1.0.0";
-        proxyJump = "proxy";
-        user = "root";
-        localForwards = [
-          {
-            bind.port = 8006;
-            host.address = "localhost";
-            host.port = 8006;
-          }
-        ];
-      };
-      "kuma" = {
-        hostname = "10.0.1.10";
-        proxyJump = "proxy";
+        proxyJump = "nixos-server";
         user = "root";
       };
-      "base" = {
-        hostname = "10.0.2.10";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "pihole" = {
-        hostname = "10.0.3.10";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "printer" = {
-        hostname = "10.0.3.20";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "home" = {
-        hostname = "10.0.3.30";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "cloud" = {
-        hostname = "10.0.4.10";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "archive" = {
-        hostname = "10.0.4.20";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "photos" = {
-        hostname = "10.0.4.30";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "pdf" = {
-        hostname = "10.0.4.40";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "bachelor" = {
-        hostname = "10.0.5.10";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "test" = {
-        hostname = "10.0.9.00";
-        proxyJump = "proxy";
-        user = "root";
-      };
-      "speedtest" = {
-        hostname = "10.0.9.10";
-        proxyJump = "proxy";
+      "nixos-server" = {
+        hostname = "ssh.davidkopczynski.com";
         user = "root";
       };
       "backup-server" = {
         hostname = "10.0.1.20";
-        proxyJump = "proxy";
+        proxyJump = "nixos-server";
         user = "root";
       };
       "workstation" = {
         hostname = "192.168.0.123";
-        proxyJump = "proxy";
+        proxyJump = "nixos-server";
         user = "user";
       };
 
