02348f6a26b52b94f1fa7160e185fd07d6c94856
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-01-22 12:10:57 +0100

    Added swap and updated boot kernel modules for workstation

--- hosts/workstation.nix
+++ hosts/workstation.nix
@@ -12,25 +12,20 @@
     <nixos-hardware/common/cpu/intel/cpu-only.nix>
   ];
 
-  # Boot parameters taken from hardware-configuration.nix
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.kernelModules = [
-    "kvm-intel"
-  ];
+  # (Tweaked) boot parameters taken from hardware-configuration.nix
+  boot.initrd.availableKernelModules = [ "nvme" ];
+  boot.kernelModules = [ "kvm-intel" ];
 
   # File systems
-  swapDevices = [ ];
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/boot";
     fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
   };
 
   fileSystems."/" = {
