d3911a82aa386e8a832e695945250d6551996efc
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-01-26 13:48:48 +0100

    Updated capslock behavior on laptop

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -57,6 +57,30 @@
 
       # Open terminal with FK12
       settings.main.media = "C-A-t";
+
+      # Disable caps lock
+      settings.main.capslock = "overload(caps, esc)";
+      settings.caps = {
+
+        # Special layer characters
+        w = "pageup";
+        a = "home";
+        s = "pagedown";
+        d = "end";
+
+        "1" = "f1";
+        "2" = "f2";
+        "3" = "f3";
+        "4" = "f4";
+        "5" = "f5";
+        "6" = "f6";
+        "7" = "f7";
+        "8" = "f8";
+        "9" = "f9";
+        "0" = "f10";
+        "ß" = "f11";
+        "´" = "f12";
+      };
     };
   };
 
