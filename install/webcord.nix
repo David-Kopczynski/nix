8d1266f013def02b45058c2adb97727bdb390ec0
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-01-30 01:21:40 +0100

    Fixed Krisp with WebCord

--- install/webcord.nix
+++ install/webcord.nix
@@ -1,5 +1,40 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [ webcord ];
+
+  home-manager.users."user".xdg.configFile."WebCord/_config.json" = {
+
+    # Copy config into place to prevent read-only errors when starting
+    onChange =
+      let
+        dir = "${config.home-manager.users."user".xdg.configHome}/WebCord";
+      in
+      ''
+        rm -f ${dir}/config.json
+        cp ${dir}/_config.json ${dir}/config.json
+        chmod u+w ${dir}/config.json
+      '';
+
+    text = builtins.toJSON {
+
+      # General settings
+      settings.advanced.redirection.warn = false;
+      settings.general.menuBar.hide = true;
+      settings.general.window.hideOnClose = false;
+
+      # General permissions
+      settings.privacy.permissions = {
+        audio = true;
+        display-capture = true;
+        fullscreen = true;
+        notifications = true;
+        video = true;
+      };
+
+      # Disable CSP for better compatibility
+      # (this also fixes Krisp)
+      settings.advanced.csp.enabled = false;
+    };
+  };
 }
