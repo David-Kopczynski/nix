2618a5acd1f008b9760f0918f260969adaf2938d
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-02-26 22:32:05 +0100

    Added signing to git

--- install/git.nix
+++ install/git.nix
@@ -8,5 +8,9 @@
 
     userName = "David E. C. Kopczynski";
     userEmail = "david.kop.dk@gmail.com";
+
+    extraConfig.gpg.format = "ssh";
+    signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPLqP71iBRAFd7OFIjlkN6yGEr++G5eRDJ+U57R9f8e user@nixos";
+    signing.signByDefault = true;
   };
 }
