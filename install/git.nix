ac6ccaa9a9a75094aa6b0c184bdcc0749e0c093e
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-03-12 11:46:15 +0100

    Fixed git merge issues

--- install/git.nix
+++ install/git.nix
@@ -9,6 +9,8 @@
     userName = "David E. C. Kopczynski";
     userEmail = "david.kop.dk@gmail.com";
 
+    extraConfig.pull.rebase = false;
+
     extraConfig.gpg.format = "ssh";
     signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPLqP71iBRAFd7OFIjlkN6yGEr++G5eRDJ+U57R9f8e user@nixos";
     signing.signByDefault = true;
