f94b0d5a4ddda381ac6422195ea065ce492b0172
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-03-14 14:10:31 +0100

    Added good practice flags to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 cd "$(dirname "$0")"
 
 # Setup correct host
-read -p "Host: " host
+read -rp "Host: " host
 echo "{ imports = [ (/. + \"$PWD/hosts/$host.nix\") (/. + \"$PWD/default.nix\") ]; }" > /etc/nixos/configuration.nix
