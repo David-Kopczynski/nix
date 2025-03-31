916620b6c5fddc438128dce79b43004214c9b544
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-01 00:56:53 +0200

    Moved vscode specific packages into its fhs

--- install/vscode.nix
+++ install/vscode.nix
@@ -10,7 +10,17 @@ in
   home-manager.users."user".programs.vscode.enable = true;
   home-manager.users."user".programs.vscode.package =
     with pkgs.unstable;
-    vscode.fhsWithPackages (ps: [ patched-openssh ]);
+    vscode.fhsWithPackages (
+      ps:
+      [ patched-openssh ]
+      ++ [
+        # Packages required by various profiles
+        nixd
+        nixfmt-rfc-style
+        python3
+        texliveFull
+      ]
+    );
 
   # Remove gnome default application
   environment.gnome.excludePackages = with pkgs; [ gnome-text-editor ];
