f2da605c12d8f4e43f5a1627123bf5a5db80cec9
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-01 22:04:48 +0200

    Added customization to bluetooth battery meter

--- install/gnome.bluetooth-battery-meter.nix
+++ install/gnome.bluetooth-battery-meter.nix
@@ -11,5 +11,11 @@
       disable-user-extensions = false;
       enabled-extensions = with pkgs; [ gnomeExtensions.bluetooth-battery-meter.extensionUuid ];
     };
+
+    # Custom settings
+    settings."org/gnome/shell/extensions/Bluetooth-Battery-Meter" = {
+      enable-battery-level-text = true;
+      level-indicator-color = 0;
+    };
   };
 }
