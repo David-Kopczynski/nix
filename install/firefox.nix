f6d668d1f55c809ad885e2412d72ff69c47b8027
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-02 20:33:08 +0200

    Added firefox as default html scheme

--- install/firefox.nix
+++ install/firefox.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.firefox.enable = true;
@@ -42,7 +42,16 @@
     "browser.translations.automaticallyPopup" = false;
   };
 
-  xdg.mime.defaultApplications."application/pdf" = "firefox-esr.desktop";
+  # Firefox as default browser
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = lib.genAttrs [
+    "application/pdf"
+    "text/html"
+    "x-scheme-handler/about"
+    "x-scheme-handler/http"
+    "x-scheme-handler/https"
+    "x-scheme-handler/unknown"
+  ] (_: "firefox-esr.desktop");
 
   # Remove gnome default application
   environment.gnome.excludePackages = with pkgs; [
