1991271278bb25fdd618da5b56ee2af3958d7c3d
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-05 13:54:48 +0200

    Added declarative wifi configuration

--- install/network.nix
+++ install/network.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   networking.hostName = "nixos-${config.system.name}";
@@ -14,4 +14,58 @@
     "1.1.1.2"
     "1.0.0.2"
   ];
+
+  # Declarative wifi configuration
+  sops.secrets."wifi.env" = { };
+  networking.networkmanager.ensureProfiles.environmentFiles = [ config.sops.secrets."wifi.env".path ];
+
+  networking.networkmanager.ensureProfiles.profiles =
+    lib.genAttrs
+      [
+        "Father"
+        "Home"
+        "Hotspot"
+        "Mother"
+      ]
+      (name: {
+        connection = {
+          id = "\$${name}_SSID";
+          type = "wifi";
+        };
+
+        wifi = {
+          mode = "infrastructure";
+          ssid = "\$${name}_SSID";
+        };
+
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "\$${name}_PSK";
+        };
+      })
+    // {
+      "University" = {
+        connection = {
+          id = "eduroam";
+          type = "wifi";
+        };
+
+        wifi = {
+          mode = "infrastructure";
+          ssid = "eduroam";
+        };
+
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-eap";
+        };
+
+        "802-1x" = {
+          eap = "pwd";
+          identity = "$University_IDENT";
+          password = "$University_PSK";
+        };
+      };
+    };
 }
