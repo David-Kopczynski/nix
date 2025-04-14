b3c431c3b449c7068e963e7ada4f1b501d9eed60
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-14 20:01:23 +0200

    Added splash screen

--- install/system.nix
+++ install/system.nix
@@ -6,6 +6,20 @@
   boot.loader.systemd-boot.configurationLimit = 16;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Splash screen
+  boot.plymouth.enable = true;
+  boot = {
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
+
   # Automatically keep system clean
   nix.gc.automatic = true;
   nix.gc.options = "--delete-older-than 14d";
