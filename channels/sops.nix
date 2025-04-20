d5b91f2af55905d2a14a273046d34bbbdc0bbf71
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-20 13:11:40 +0200

    Fixed sops-nix user SSH age key generation

--- channels/sops.nix
+++ channels/sops.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ <sops-nix/modules/sops> ];
@@ -6,8 +6,23 @@
   sops.defaultSopsFile = ../resources/sops/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.generateKey = true;
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  sops.age.sshKeyPaths = [ "/home/user/.ssh/id_ed25519" ];
+  sops.age.sshKeyPaths = [ "${config.home-manager.users."user".home.homeDirectory}/.ssh/id_ed25519" ];
   sops.gnupg.sshKeyPaths = [ ];
+
+  # Generate age keys with correct owner and permissions
+  # Required to edit secrets via `sops` command
+  home-manager.users."user" =
+    { lib, ... }:
+    {
+      home.activation."ssh-to-age" = lib.hm.dag.entryAfter [ "sshActivatioinAction" ] ''
+        AGE_HOME="''${XDG_CONFIG_HOME:-$HOME/.config}/sops/age"
+
+        mkdir -p $AGE_HOME
+        : > $AGE_HOME/keys.txt
+
+        for sshKey in ${builtins.concatStringsSep " " config.sops.age.sshKeyPaths}; do
+          ${with pkgs; ssh-to-age}/bin/ssh-to-age -private-key -i $sshKey >> "$AGE_HOME/keys.txt"
+        done
+      '';
+    };
 }
