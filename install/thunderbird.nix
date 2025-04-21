7ffd1886a66daf65f483c12678c24e259a369fb3
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-21 19:50:16 +0200

    Added declarative mails

--- install/thunderbird.nix
+++ install/thunderbird.nix
@@ -1,7 +1,51 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ thunderbird ];
+  home-manager.users."user".programs.thunderbird.enable = true;
+  home-manager.users."user".programs.thunderbird = {
+
+    profiles."default".isDefault = true;
+  };
+
+  # Mails
+  home-manager.users."user".accounts.email.accounts."Personal" = {
+    primary = true;
+
+    address = "mail@davidkopczynski.com";
+    realName = "David Kopczynski";
+
+    imap.host = "imap.ionos.de";
+    imap.port = 993;
+    smtp.host = "smtp.ionos.de";
+    smtp.port = 587;
+    smtp.tls.useStartTls = true;
+    userName = "mail@davidkopczynski.com";
+
+    thunderbird.enable = true;
+  };
+  home-manager.users."user".accounts.email.accounts."Google" = {
+
+    address = "david.kop.dk@gmail.com";
+    realName = "David Kopczynski";
+
+    flavor = "gmail.com";
+
+    thunderbird.enable = true;
+  };
+  home-manager.users."user".accounts.email.accounts."RWTH Aachen" = {
+
+    address = "david.kopczynski@rwth-aachen.de";
+    realName = "David Kopczynski";
+
+    imap.host = "mail.rwth-aachen.de";
+    imap.port = 993;
+    smtp.host = "mail.rwth-aachen.de";
+    smtp.port = 587;
+    smtp.tls.useStartTls = true;
+    userName = "hg066732@rwth-aachen.de";
+
+    thunderbird.enable = true;
+  };
 
   # Remove gnome default application
   environment.gnome.excludePackages = with pkgs; [ geary ];
