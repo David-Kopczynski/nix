198e8fc6d3bab851b260b71d7c49e5feadc8c25b
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-04-29 13:02:11 +0200

    Added gamescope and updated gamemode

--- install/gamemode.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  # GameMode can be enabled with `gamemoderun ./game`
-  # some applications may wrap the game automatically, while others like steam require `gamemoderun %command%`
-  programs.gamemode.enable = true;
-
-  users.users."user".extraGroups = [ "gamemode" ];
-}
--- install/steam.nix
+++ install/steam.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.steam.enable = true;
@@ -9,4 +9,20 @@
     localNetworkGameTransfers.openFirewall = true;
     remotePlay.openFirewall = true;
   };
+
+  # Gaming Optimizations
+  # Simply add to Steam `LAUNCH OPTIONS` or start game with: "gaming-mode %command%"
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "gaming-mode";
+      text = ''
+        gamescope -W 2560 -H 1440 -r 165 -f --adaptive-sync --immediate-flips --rt -- gamemoderun "$@"
+      '';
+    })
+  ];
+
+  programs.gamemode.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+
+  users.users."user".extraGroups = [ "gamemode" ];
 }
