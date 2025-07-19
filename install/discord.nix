737bee48ce056f4c6e948e98737b5e8060a125da
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-07-19 22:49:26 +0200

    Updated discord and notion image to builtin fetcher

--- install/discord.nix
+++ install/discord.nix
@@ -7,9 +7,9 @@
       desktopName = "Discord";
       url = "https://discord.com/app";
       description = "All-in-one cross-platform voice and text chat for gamers";
-      icon = pkgs.fetchurl {
-        url = "https://github.com/github/explore/blob/main/topics/discord/discord.png";
-        sha256 = "sha256-05Ln/r7NIBKLIQBlaCNo9EKRWvIMGdHkn0TISZAVKXw=";
+      icon = builtins.fetchurl {
+        name = "discord.png";
+        url = "https://github.com/github/explore/blob/main/topics/discord/discord.png?raw=true";
       };
     })
   ];
--- install/notion.nix
+++ install/notion.nix
@@ -7,9 +7,9 @@
       desktopName = "Notion";
       url = "https://www.notion.so/";
       description = "App to write, plan, collaborate, and get organised";
-      icon = pkgs.fetchurl {
-        url = "https://github.com/github/explore/blob/main/topics/notion/notion.png";
-        sha256 = "sha256-qWiOBbfCMYa5VpBqvTknhH1u0oEcaZm9d+QZSX1Fjrs=";
+      icon = builtins.fetchurl {
+        name = "notion.png";
+        url = "https://github.com/github/explore/blob/main/topics/notion/notion.png?raw=true";
       };
     })
   ];
