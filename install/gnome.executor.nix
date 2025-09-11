085e4878455ecbe37257f01269e2ab937bb578a1
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-09-11 22:50:43 +0200

    Removed Toggl Track

--- install/gnome.executor.nix
+++ install/gnome.executor.nix
@@ -21,39 +21,9 @@
     settings."org/gnome/shell/extensions/executor" = {
 
       center-active = true;
-      center-commands-json = builtins.toJSON {
-        commands = [
-          {
-            isActive = true;
-            command = "${
-              pkgs.writeShellApplication {
-
-                name = "work-balance";
-                runtimeInputs = with pkgs; [ curl ] ++ [ jq ];
-                text = ''
-                  result=$(curl \
-                    --silent --retry 9 --retry-delay 60 --retry-connrefused --max-time 10 \
-                    -u "$(cat ${config.sops.secrets."toggl".path}):api_token" -H "Content-Type: application/json" \
-                    "https://api.track.toggl.com/api/v9/me/time_entries?start_date=2025-06-01&end_date=$(date -d tomorrow +%Y-%m-%d)" \
-                  ) || result="unavailable"
-
-                  daily=$(echo "$result" | jq '[.[].duration] | add / 3600 | round')           || daily=0
-                  days=$(echo "$result"  | jq '[.[].start | split("T")[0]] | unique | length') || days=0
-                  diff=$((daily - days * 8))
-
-                  if [ "$result" = "unavailable" ]; then
-                    echo "Toggl API unavailable"
-                  else
-                    echo "Work balance: $((diff))h"
-                  fi
-                '';
-              }
-            }/bin/work-balance";
-            interval = 3600;
-            uuid = "alarm";
-          }
-        ];
-      };
+      center-commands-json = with lib.gvariant; [
+        (mkDictionaryEntry "commands" [ (mkEmptyArray (type.dictionaryEntryOf type.string type.string)) ])
+      ];
       center-index = 10;
 
       left-active = false;
@@ -68,10 +38,5 @@
 
       click-on-output-active = false;
     };
-  };
-
-  # Secrets for toggle track
-  sops.secrets."toggl" = {
-    owner = config.users.users."user".name;
   };
 }
