5988ab643d76aed8e0c377c642fee7c47106a05f
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-11-02 14:57:57 +0100

    Updated formatting

--- default.nix
+++ default.nix
@@ -2,12 +2,7 @@
 
 {
   # Automatically install all configuration from channel and install directory
-  imports = (
-    builtins.concatMap
-      (dir: builtins.map (n: toString dir + "/${n}") (builtins.attrNames (builtins.readDir dir)))
-      [
-        ./channels
-        ./install
-      ]
-  );
+  imports = builtins.concatMap (
+    dir: builtins.map (n: toString dir + "/${n}") (builtins.attrNames (builtins.readDir dir))
+  ) ([ ./channels ] ++ [ ./install ]);
 }
