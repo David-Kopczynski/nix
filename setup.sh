39ea1b7c5959338fff480495bf9c3825513bfed5
David E. C. Kopczynski <david.kop.dk@gmail.com>
2025-11-02 14:59:56 +0100

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
 
-cd "$(dirname "$0")"
-
-# Setup correct host
 read -rp "Host: " host
-echo "{ imports = [ (/. + \"$PWD/hosts/$host.nix\") (/. + \"$PWD/default.nix\") ]; }" > /etc/nixos/configuration.nix
+echo "{ imports = [ (/. + \"$(realpath $(dirname "$0"))/hosts/$host.nix\") (/. + \"$(realpath $(dirname "$0"))/default.nix\") ]; }" > /etc/nixos/configuration.nix
