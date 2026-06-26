d16921d9733a17c4c658a35678df6eb11f72ad0d
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-06-27 00:43:00 +0200

    refactor: migrated fonts.nix to new structure

---
+++ install/fonts/users/user/default.nix
@@ -0,0 +1,11 @@
+{ user }:
+
+{
+  home-manager.users.${user} = { pkgs, ... }: {
+    home.packages = with pkgs; [ fira-code ] ++ [ monocraft ];
+
+    # Default fonts
+    fonts.fontconfig.defaultFonts.monospace = [ "FiraCode" ] ++ [ "Monocraft" ];
+    dconf.settings."org/gnome/desktop/interface".monospace-font-name = "Monocraft 8";
+  };
+}
