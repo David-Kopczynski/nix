2adde74294df2fa12b388de84fe838e04baef39d
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-06-26 21:23:21 +0200

    refactor: migrated nav.nix to new structure

--- install/nav/default.nix
+++ install/nav/default.nix
@@ -3,5 +3,6 @@
 {
   environment.systemPackages = with pkgs; [ nav ];
 
+  # Shell hook for additional features
   environment.interactiveShellInit = ''eval "$(nav --init "$(basename "$SHELL")")"'';
 }
