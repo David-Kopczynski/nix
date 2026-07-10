547099e96eb8988d5924910a253819424376a709
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-07-10 21:22:22 +0200

    chore: disabled "git.suggestSmartCommit" in vscodium
    This changes the source control button behavior to be more usable imo.

--- install/vscodium/users/user/default.nix
+++ install/vscodium/users/user/default.nix
@@ -27,6 +27,7 @@
           # Git
           "git.autofetch" = true;
           "git.confirmSync" = false;
+          "git.suggestSmartCommit" = false;
 
           # Theming
           "window.autoDetectColorScheme" = true;
