968cc54102422c69631eca45e48a70ada5cc8017
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-07-24 18:45:04 +0200

    chore: moved howdy to workstation only
    As it seems the framework 13 does not support howdy. Migrated howdy for workstation only.

--- hosts/workstation/extra/howdy/default.nix
+++ hosts/workstation/extra/howdy/default.nix
@@ -1,6 +1,17 @@
 { ... }:
 
 {
+  services.howdy.enable = true;
+  services.howdy = {
+
+    # General
+    control = "sufficient";
+  };
+
+  # Hardware support
+  services.linux-enable-ir-emitter.enable = true;
+
+  # Secrets
   sops.secrets."workstation/howdy/user.dat" = {
 
     sopsFile = ./user.dat;
