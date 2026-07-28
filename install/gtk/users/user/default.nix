724de65ccbc56a8a66ea8e04800eedbaf39b1602
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-07-28 19:46:57 +0200

    chore: removed adw-gtk3-dark
    This styling broke lots of applications and has been removed. However, pavucontrol still seems to have issues with native Adwaita. An issue is available here: https://gitlab.freedesktop.org/pulseaudio/pavucontrol/-/work_items/159

--- install/gtk/users/user/default.nix
+++ install/gtk/users/user/default.nix
@@ -1,16 +1,14 @@
 { user }:
 
 {
-  home-manager.users.${user} = { config, pkgs, ... }: {
+  home-manager.users.${user} = { ... }: {
 
     gtk.enable = true;
     gtk = {
 
       # General
       colorScheme = "dark";
-      theme.name = "adw-gtk3-dark";
-      theme.package = with pkgs; adw-gtk3;
-      gtk4.theme = config.gtk.theme;
+      gtk4.theme = null;
     };
   };
 }
