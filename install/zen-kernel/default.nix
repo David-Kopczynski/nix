58597d87212b200c5610ab4b156a4f975aaaf9d1
David E. C. Kopczynski <mail@davidkopczynski.com>
2026-09-19 11:13:41 +0200

    chore: updated system
    This update had some regressions that needed to be fixed for the current time being.

--- install/zen-kernel/default.nix
+++ install/zen-kernel/default.nix
@@ -1,5 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   boot.kernelPackages = with pkgs.linuxKernel.packages; linux_zen;
+
+  # TODO Remove regression once patched
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "595.91.07";
+    sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+    sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
+    openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+    settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
+    persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
+  };
+
+  # TODO Remove regression once patched
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Logitech G502 X Plus]
+    MatchName=Logitech G502 X PLUS
+    AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES
+  '';
 }
